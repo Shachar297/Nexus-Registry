a5c92c42132e6bee2e7410b90def2c867a98565d
Shachar297 <thisishachar@gmail.com>
2022-06-28 12:23:06 +0300

    Initial Nexus Commit

--- .history/bash/creds_20220628111735.sh
+++
@@ -1,8 +0,0 @@
-# Get Nexus admin initial password.
-
-current
-
-containerName="installations_nexus_1"
-nexusContainerID=$(docker ps -aq --filter="NAME=$containerName")
-export userPass=$(docker container exec $nexusContainerID cat nexus-data/admin.password; echo)
-echo $userPass
